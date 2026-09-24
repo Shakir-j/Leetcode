8a5e8fedcea5467492367265ea72d1997a9c5e88
Shakir Jamal <shakirj308@gmail.com>
2026-09-24 19:30:06 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.86%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int cnt = 0;
+        while(n > 1) {
+            cnt += n & 1; // (n & 1) is the odd check
+            n = n >> 1; // This is same as (n/2)
+        }
+
+        if(n == 1) cnt += 1;
+
+        return cnt;
+    }
+};
