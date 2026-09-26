70adb40320af58f6d4b7f4129b854c719473318d
Shakir Jamal <shakirj308@gmail.com>
2026-09-26 18:54:19 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.5%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -16,5 +17,23 @@ public:
         }
 
         return -1;
+    }
+};
+*/
+
+// Using Bit Manipulation
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int XOR = 0;
+
+        for(int i = 0; i < n; i++) {
+            XOR = XOR ^ nums[i];
+        }
+
+        return XOR;
+    
     }
 };
